01eb0d7e834a029c9b29fc349459f01565536d44
Ewelina Ziarkowska <124273313+ziarkowskaev@users.noreply.github.com>
2024-07-07 21:49:09 +0300

    commit part7.7

--- src/App.jsx
+++ src/App.jsx
@@ -1,216 +1,82 @@
-import { useState } from "react";
-import { useField } from "./hooks";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  Navigate,
-  useParams,
-  useNavigate,
-  useMatch,
-} from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const Menu = () => {
-  const padding = {
-    paddingRight: 5,
-  };
-  return (
-    <div>
-      <Link style={padding} to="/">
-        anecdotes
-      </Link>
-      <Link style={padding} to="/create">
-        create new
-      </Link>
-      <Link style={padding} to="/about">
-        about
-      </Link>
-    </div>
-  );
-};
+const useField = (type) => {
+  const [value, setValue] = useState('')
 
-const AnecdoteList = ({ anecdotes }) => (
-  <div>
-    <h2>Anecdotes</h2>
-    <ul>
-      {anecdotes.map((anecdote) => (
-        <li key={anecdote.id}>
-          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
-const Anecdote = ({ anecdote }) => {
-  const link = anecdote.info;
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
 
-  return (
-    <div>
-      <h2>
-        {anecdote.content} by {anecdote.author}
-      </h2>
-      <div>has {anecdote.votes} votes</div>
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {
+    if (name) {
+      axios
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+        .then(response => {
+          setCountry({ data: response.data, found:true})
+        })
+        .catch(error => {
+          setCountry({ data: null,found:false })
+        })
+    }
+  }, [name])
+
+  return country
+}
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null
+  }
+
+  if (!country.found) {
+    return (
       <div>
-        for more info see{" "}
-        <a href={link} target="_blank" rel="noopener noreferrer">
-          {link}
-        </a>
+        not found...
       </div>
-    </div>
-  );
-};
-
-const About = () => (
-  <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
-
-    <em>
-      An anecdote is a brief, revealing account of an individual person or an
-      incident. Occasionally humorous, anecdotes differ from jokes because their
-      primary purpose is not simply to provoke laughter but to reveal a truth
-      more general than the brief tale itself, such as to characterize a person
-      by delineating a specific quirk or trait, to communicate an abstract idea
-      about a person, place, or thing through the concrete details of a short
-      narrative. An anecdote is "a story with a point."
-    </em>
-
-    <p>
-      Software engineering is full of excellent anecdotes, at this app you can
-      find the best and add more.
-    </p>
-  </div>
-);
-
-const Footer = () => (
-  <div>
-    Anecdote app for <a href="https://fullstackopen.com/">Full Stack Open</a>.
-    See{" "}
-    <a href="https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js">
-      https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js
-    </a>{" "}
-    for the source code.
-  </div>
-);
-
-const CreateNew = (props) => {
-  const content = useField("text");
-  const author = useField("text");
-  const info = useField("text");
-  const navigate = useNavigate();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    props.addNew({
-      content: content.value,
-      author: author.value,
-      info: info.value,
-      votes: 0,
-    });
-
-    navigate("/");
-  };
-
-  const handleReset = () => {
-    content.onReset();
-    author.onReset();
-    info.onReset();
-  };
+    )
+  }
 
   return (
     <div>
-      <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          content
-          <input {...content} />
-        </div>
-        <div>
-          author
-          <input {...author} />
-        </div>
-        <div>
-          url for more info
-          <input {...info} />
-        </div>
-        <button type="submit">create</button>
-        <button type='button'onClick={handleReset}>reset</button>
-      
-      </form>
-      
+      <h3>{country.data.name.common} </h3>
+      <div>capital {country.data.capital} </div>
+      <div>population {country.data.population}</div> 
+      <img src={country.data.flags.png} height='100' alt={`flag of ${country.data.name}`}/>  
     </div>
-  );
-};
+  )
+}
 
 const App = () => {
-  const [anecdotes, setAnecdotes] = useState([
-    {
-      content: "If it hurts, do it more often",
-      author: "Jez Humble",
-      info: "https://martinfowler.com/bliki/FrequencyReducesDifficulty.html",
-      votes: 0,
-      id: 1,
-    },
-    {
-      content: "Premature optimization is the root of all evil",
-      author: "Donald Knuth",
-      info: "http://wiki.c2.com/?PrematureOptimization",
-      votes: 0,
-      id: 2,
-    },
-  ]);
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
 
-  const [notification, setNotification] = useState("");
-
-  const addNew = (anecdote) => {
-    anecdote.id = Math.round(Math.random() * 10000);
-    setAnecdotes(anecdotes.concat(anecdote));
-    setNotification(`a new anecdote ${anecdote.content} created!`);
-
-    setTimeout(() => {
-      setNotification("");
-    }, 5000);
-  };
-
-  const anecdoteById = (id) => anecdotes.find((a) => a.id === id);
-
-  const vote = (id) => {
-    const anecdote = anecdoteById(id);
-
-    const voted = {
-      ...anecdote,
-      votes: anecdote.votes + 1,
-    };
-
-    setAnecdotes(anecdotes.map((a) => (a.id === id ? voted : a)));
-  };
-
-  const match = useMatch("/anecdotes/:id");
-
-  const anecdote = match
-    ? anecdotes.find((anecdote) => anecdote.id === Number(match.params.id))
-    : null;
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
 
   return (
     <div>
-      <h1>Software anecdotes</h1>
-      <Menu />
-      <div>{notification}</div>
-      <Routes>
-        <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/create" element={<CreateNew addNew={addNew} />} />
-        <Route
-          path="/anecdotes/:id"
-          element={<Anecdote anecdote={anecdote} />}
-        />
-      </Routes>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
 
-      <Footer />
+      <Country country={country} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
